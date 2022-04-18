07d1e0235294182ebd5bdf0a65d278d4c5667f9f
learngitshruti <101583655+learngitshruti@users.noreply.github.com>
2022-04-18 14:10:40 +0530

    All department class

---
+++ hrDepartment.java
@@ -0,0 +1,20 @@
+package com.greatlearning.model;
+
+public class hrDepartment extends superDepartment {
+
+	public String departmentName() {
+		return "Hr Department ";
+	}
+	
+	public String getTodaysWork() {
+		return "Fill today’s worksheet and mark your attendance";
+	}
+	
+	public String getWorkDeadline() {
+		return "Complete by EOD";
+	}
+	
+	public String doActivity() {
+		return "team Lunch";
+	}
+}
---
+++ superDepartment.java
@@ -0,0 +1,20 @@
+package com.greatlearning.model;
+
+public class superDepartment {
+
+	public String departmentName() {
+		return "Super Departmemt";
+	}
+	
+	public String getTodaysWork() {
+		return "No work as of now";
+	}
+	
+	public String getWorkDeadline() {
+		return "Nil";
+	}
+	
+	public String isTodayAHoliday() {
+		return "Today is not a Holiday";
+	}
+}
